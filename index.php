b4323e55facdf3994d5b361dc2bc592e4e3c3162
Vijayendra Kumar Singh <vijayendra.kumar2015@vit.ac.in>
2017-10-10 03:21:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php
- echo '<html><body><center><h1 style="margin-top:50%">Blood Help India</h1>
- 
- <a href="#">get the app here</a>
- 
- </center></body></html>';
+
+echo '<html>
+
+<body>
+
+<center>
+
+<h1 style="font:Helvetica">Blood Help India</h1>
+
+<a href=""><h3>Get the app here</h3></a>
+
+</center>
+
+</body>
+
+</html>';
+
 ?>

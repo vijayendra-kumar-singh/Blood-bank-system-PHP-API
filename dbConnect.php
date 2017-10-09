195f84bc81bc2b9076b35ac5f2553437eb43bdd6
Vijayendra Kumar Singh <vijayendra.kumar2015@vit.ac.in>
2017-10-09 18:39:43 +0530

    Update dbConnect.php

--- dbConnect.php
+++ dbConnect.php
@@ -1,14 +1,14 @@
 <?php
-	// define('HOST','sql12.freesqldatabase.com');
-	// define('USER','sql12198332');
-	// define('PASS','yLYISJGiAc');
-	// define('DB','sql12198332');
-	
-	// $con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
-
-	define('HOST','localhost');
-	define('USER','root');
-	define('PASS','');
-	define('DB','blood_bbank');
+	define('HOST','sql12.freesqldatabase.com');
+	define('USER','sql12198332');
+	define('PASS','yLYISJGiAc');
+	define('DB','sql12198332');
 	
 	$con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');
+
+// 	define('HOST','localhost');
+// 	define('USER','root');
+// 	define('PASS','');
+// 	define('DB','blood_bbank');
+	
+// 	$con = mysqli_connect(HOST,USER,PASS,DB) or die('Unable to Connect');

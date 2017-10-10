19a3a97d654c57c08a9308b01b4c08f1edf04c9a
Vijayendra Kumar Singh <vijayendra.kumar2015@vit.ac.in>
2017-10-10 22:28:08 +0530

    Update register.php

--- register.php
+++ register.php
@@ -8,12 +8,13 @@
         $btype = $_POST['btype'];
         $utype = $_POST['utype'];
         $token = $_POST['token'];
+        $sex = $_GET['sex'];
 
         $password = password_hash($pass, PASSWORD_DEFAULT);
 
 		require_once('dbConnect.php');
 
-        $sql = "INSERT INTO users (name,number,age,pincode,password,btype,utype,token) VALUES ('$name','$number','$age','$pincode','$password','$btype','$utype', '$token')";
+        $sql = "INSERT INTO users (name,number,age,pincode,password,btype,utype,token,sex) VALUES ('$name','$number','$age','$pincode','$password','$btype','$utype', '$token', '$sex')";
 
 		if(mysqli_query($con,$sql)){
 			echo json_encode(array('status'=>"success", 'result'=>$utype));

2cc0484bdc8b52afc50c4a578ce95a0b5488f34f
Vijayendra Kumar Singh <vijayendra.kumar2015@vit.ac.in>
2017-10-12 17:35:35 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,24 +1,28 @@
 <?php
 
-        $name = $_POST['name'];
-        $number = $_POST['number'];
-        $age = $_POST['age'];
-        $pincode = $_POST['pincode'];
-        $pass = $_POST['password'];
-        $btype = $_POST['btype'];
-        $utype = $_POST['utype'];
-        $token = $_POST['token'];
-        $sex = $_GET['sex'];
+	$name = $_POST['name'];
+	$number = $_POST['number'];
+	$age = $_POST['age'];
+	$pincode = $_POST['pincode'];
+	$pass = $_POST['password'];
+	$btype = $_POST['btype'];
+	$utype = $_POST['utype'];
+	$token = $_POST['token'];
+	$sex = $_POST['sex'];
 
-        $password = password_hash($pass, PASSWORD_DEFAULT);
+	$password = password_hash($pass, PASSWORD_DEFAULT);
 
-		require_once('dbConnect.php');
+	require_once('dbConnect.php');
 
-        $sql = "INSERT INTO users (name,number,age,pincode,password,btype,utype,token,sex) VALUES ('$name','$number','$age','$pincode','$password','$btype','$utype', '$token', '$sex')";
+	$sql = "INSERT INTO users (name,number,age,pincode,password,btype,utype,token,sex) VALUES ('$name','$number','$age','$pincode','$password','$btype','$utype', '$token', '$sex')";
 
-		if(mysqli_query($con,$sql)){
-			echo json_encode(array('status'=>"success", 'result'=>$utype));
-		}else{
-			echo json_encode(array('status'=>"fail", 'result'=>"server error"));
-		}
-		mysqli_close($con);
+	if(mysqli_query($con,$sql)){
+		
+		echo json_encode(array('status'=>"success", 'result'=>$utype));
+		
+	}else{
+		
+		echo json_encode(array('status'=>"fail", 'result'=>"server error"));
+		
+	}
+	mysqli_close($con);
